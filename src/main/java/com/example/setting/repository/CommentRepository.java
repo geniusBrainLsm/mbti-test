4e9f6cbdbbb3aa6f3f08cf705b7cbf84cbd087cd
jeonggiwan <rldhks10101@gmail.com>
2023-10-18 21:21:23 +0900

    댓ê댓글 삭제 기능 추가

--- src/main/java/com/example/setting/repository/CommentRepository.java
+++ src/main/java/com/example/setting/repository/CommentRepository.java
@@ -1,13 +1,16 @@
 package com.example.setting.repository;
 
 import com.example.setting.entity.Comment;
+import com.example.setting.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByMemberMbti(String memberMbti);
     List<Comment> findByMemberNickname(String memberNickname);
+
 }
